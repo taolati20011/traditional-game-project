6af0970da725bf2a4bfa2d6f6ec1d516ec8f29c8
taolati20011 <anhdung.cvp@gmail.com>
2022-11-03 18:09:29 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,33 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import './css/style.css';
 import './css/o-an-quan-style.css';
-import Page from './html/o-an-quan.js';
-import Home from './html/index.js';
+import Page from './html/Page.js';
+import Home from './html/Home.js';
 
 const App = () => {
-
-  const [groups, setGroups] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const URL = "api/game/get-detail/" + "?game-id=5";
-
-  useEffect(() => {
-    setLoading(true);
-
-    fetch(URL)
-      .then(response => response.json())
-      .then(data => {
-        setGroups(data);
-        setLoading(false);
-      })
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
   return (
-    Page(groups.gameName, groups.gameDescription)
+    <Home></Home>
   );
 }
 
---
+++ src/pages/pageGame.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import './css/style.css';
+import './css/o-an-quan-style.css';
+import Page from './html/o-an-quan.js';
+import Home from './html/index.js';
+
+const gamePage = () => {
+
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const URL = "api/game/get-detail/" + "?game-id=5";
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        setGroups(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    Page(groups.gameName, groups.gameDescription)
+  );
+}
+
+export default gamePage;
