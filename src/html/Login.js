2506dcaf707b155a60c85ebefe58693da97c06bf
quanghoang1101 <quanghoangnhat01@gmail.com>
2022-11-25 14:18:06 +0700

    sua ti css

--- src/html/Login.js
+++ src/html/Login.js
@@ -5,21 +5,23 @@ import avatar from './images/avatar.png';
 const Login = () => {
     return (
         <body>
-    <img class="image" src={background}/>
-    <div class="loginbox">
-        <img src={avatar} class="avatar"/>
-        <h1>Login Here</h1>
-        <form>
-            <p>Username</p>
-            <input type="text" name="" placeholder="Enter Username"/>
-            <p>Password</p>
-            <input type="password" name="" placeholder="Enter Password"/>
-            <input type="submit" name="" value="login"/>
-            <a href="#">Forget your password?</a><br/>
-            <a href="#">Dont have an account?</a>
-        </form>
-    </div>
-</body>
+            <div class="login-body-container">
+                <img class="image" src={background} />
+                <div class="loginbox">
+                    <img src={avatar} class="avatar" />
+                    <h1>Login Here</h1>
+                    <form>
+                        <p>Username</p>
+                        <input type="text" name="" placeholder="Enter Username" />
+                        <p>Password</p>
+                        <input type="password" name="" placeholder="Enter Password" />
+                        <input type="submit" name="" value="login" />
+                        <a href="#">Forget your password?</a><br />
+                        <a href="#">Dont have an account?</a>
+                    </form>
+                </div>
+            </div>
+        </body>
     );
 }
 
