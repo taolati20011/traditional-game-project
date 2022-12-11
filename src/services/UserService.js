1ddb5548faffe8263df109f1be50c71f5bf5ea42
dungp123 <19021246@vnu.edu.vn>
2022-12-11 23:42:36 +0700

    update author

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,6 +4,7 @@ const USER_LIST_BASE_URL = "http://localhost:8080/api/user/view-all?pageSize=5";
 
 class UserService {
     getUsers() {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`;
         return axios.get(USER_LIST_BASE_URL);
     }
 }
