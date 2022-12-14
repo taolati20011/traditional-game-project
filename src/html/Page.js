0705b0b89226c961bb06ef6a5d2cd00fe6e2c8c8
quanghoang1101 <quanghoangnhat01@gmail.com>
2022-12-14 21:04:52 +0700

    ye

--- src/html/Page.js
+++ src/html/Page.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import fbIcon from './images/124010.png';
-import gameIcon from './images/images.jpg';
+
 
 class Page extends Component {
   constructor(props) {
@@ -24,71 +24,76 @@ class Page extends Component {
   }
 
   logOut() {
-      localStorage.removeItem("username")
-      localStorage.removeItem("accessToken")
+    localStorage.removeItem("username")
+    localStorage.removeItem("accessToken")
   }
 
   renderPage(props) {
-    const {currentUser} = this.state;
+    const { currentUser } = this.state;
     var urlGame = null;
+    var gameIcon = "../html/images/not-available.png";
     switch (props.gameName) {
       case "Ô ăn quan":
         urlGame = "http://127.0.0.1:5500/src/games/o-an-quan/index.html";
+        gameIcon = "../html/images/o-an-quan.jpg";
         break;
       case "Lật bài":
         urlGame = "http://127.0.0.1:5500/src/games/flipCard/quiz14.html";
-        break;    
+        gameIcon = "../html/images/lat-bai.png";
+        break;
       case "Cờ toán Việt Nam":
         urlGame = "http://127.0.0.1:5500/src/games/chess_vn/index.html";
+        gameIcon = "../html/images/co-toan.jpg";
         break;
       case "Tíc tắc toe":
         urlGame = "http://127.0.0.1:5500/src/games/tic-tac-toe/index.html";
+        gameIcon = "../html/images/co-caro.png";
         break;
-      }
+    }
     return (
       <body class="page-body">
-      <div id="head-wrapper">
-        <div class="logo">
-          <i class="game-logo">
-            <a href="/"> <img src={fbIcon} /> </a>
-          </i>
-          <p>The Game Zone</p>
-        </div>
-        <ul class="navbar">
-          <li><a href="/" class="active"> Home </a></li>
-          <li><a href="#">Tình yêu</a></li>
-          <li><a href="#">Phong tục</a></li>
-          <li><a href="#">Chiến trận</a></li>
-          <li><a href="#">Trí tuệ</a></li>
-          <li><a href="#">Nghề nghiệp</a></li>
-        </ul>
-        {currentUser ? (
-              <div class="hello-user"> 
-                <p> Hello, {currentUser}  <br></br>
+        <div id="head-wrapper">
+          <div class="logo">
+            <i class="game-logo">
+              <a href="/"> <img src={fbIcon} /> </a>
+            </i>
+            <p>The Game Zone</p>
+          </div>
+          <ul class="navbar">
+            <li><a href="/" class="active"> Home </a></li>
+            <li><a href="#">Tình yêu</a></li>
+            <li><a href="#">Phong tục</a></li>
+            <li><a href="#">Chiến trận</a></li>
+            <li><a href="#">Trí tuệ</a></li>
+            <li><a href="#">Nghề nghiệp</a></li>
+          </ul>
+          {currentUser ? (
+            <div class="hello-user">
+              <p> Hello, {currentUser}  <br></br>
                 <a class="log-out" onClick={this.logOut} href="/"> Sign out </a> </p>
-              </div>
-            ) : (<button type="button" id="btn"><a href="/login"><i class="play-logo"></i>Login</a></button>)} 
-      </div>
-      <div id="content-wrapper" class="flex-row-justify-between">
-        <div id="content-left" class="flex-col-align-center">
-          <div class="image-container">
-            <img src={gameIcon} alt="game_pic"/>
+            </div>
+          ) : (<button type="button" id="btn"><a href="/login"><i class="play-logo"></i>Login</a></button>)}
+        </div>
+        <div id="content-wrapper" class="flex-row-justify-between">
+          <div id="content-left" class="flex-col-align-center">
+            <div class="image-container">
+              <img src={gameIcon} alt="game_pic" />
+            </div>
+            <div class="button-container">
+              {urlGame ? (
+                <button class="content-button">
+                  <a href={urlGame}>Chơi game
+                  </a>
+                </button>
+              ) : (<button class="content-button-black"> <a>Chơi game</a></button>)}
+            </div>
           </div>
-          <div class="button-container">
-            {urlGame ? (
-              <button class="content-button">
-              <a href={urlGame}>Chơi game
-              </a>
-            </button>
-            ) : (<button class="content-button-black"> <a>Chơi game</a></button>)}
+          <div id="content-right" class="flex-col-align-center">
+            <h1>Mô tả {props.gameName}</h1>
+            <p>{props.description}</p>
           </div>
         </div>
-        <div id="content-right" class ="flex-col-align-center">
-          <h1>Mô tả {props.gameName}</h1>
-          <p>{props.description}</p>
-        </div>
-      </div>
-      <footer>
+        <footer>
           <div class="footer-vertical flex-row-justify-between">
             <div class="vertical-child-left">
             </div>
@@ -100,8 +105,8 @@ class Page extends Component {
             <span ><a class="footer-a">Terms of Service</a> | <a class="footer-a">Privacy Policy</a></span>
           </div>
         </footer>
-    </body>
-      );
+      </body>
+    );
   }
 
   render() {
