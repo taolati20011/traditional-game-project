fa71c2423f96eb660236dea88ac70ab8b65d2fd4
dungp123 <19021246@vnu.edu.vn>
2023-01-31 13:18:37 +0700

    method for the add game tool

--- src/login/AddGame.js
+++ src/login/AddGame.js
@@ -19,6 +19,32 @@ export default class Addgame extends Component {
         this.setState({ [event.target.name]: event.target.value })
     }
 
+    addNewGame = () => {
+        const token = localStorage.getItem("accessToken");
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Bearer ${token}`);
+
+        var raw = JSON.stringify({
+        "gameName": this.state.gameName,
+        "typeId": this.state.gameType,
+        "gameDescription": this.state.gameDescription
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            body: raw,
+            redirect: 'follow',
+            headers: myHeaders
+        };
+
+        fetch("http://localhost:8080/api/game/add", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            alert(result)
+        })
+    }
+
     render() {
         return (
             <div class="addGame-form-container">
@@ -28,7 +54,7 @@ export default class Addgame extends Component {
                     <h1>Thêm trò chơi</h1>
                     <form onSubmit={e => {
                         e.preventDefault();
-                        this.Addgame();
+                        this.addNewGame();
                     }}>
                         <div>
                             <label for="gameName">Tên trò chơi:</label>
@@ -36,19 +62,19 @@ export default class Addgame extends Component {
                         </div>
                         <div>
                             <label for="gameType">Thể loại:</label><br />
-                            <select name="gameType">
+                            <select name="gameType" onChange={this.setParams} value={this.state.gameType}>
                                 <option hidden>Chọn thể loại</option>
-                                <option value="tinhYeu">Tình yêu</option>
-                                <option value="triTue">Trí tuệ</option>
-                                <option value="phongTuc">Phong tục</option>
-                                <option value="chienTran">Chiến trận</option>
-                                <option value="ngheNghiep">Nghề nghiệp</option>
+                                <option value="1">Tình yêu</option>
+                                <option value="2">Phong tục</option>
+                                <option value="3">Chiến trận</option>
+                                <option value="4">Nghề nghiệp</option>
+                                <option value="5">Trí tuệ</option>
                             </select>
                         </div>
 
                         <div>
                             <label for="gameDescription">Mô tả:</label>
-                            <input type="text" name="gameDescription" onChange={this.setParams} placeholder="Thông tin trò choi"></input>
+                            <input type="text" name="gameDescription" onChange={this.setParams} placeholder="Thông tin trò chơi"></input>
                         </div>
                         <div>
                             <label for="gameImage">Hình ảnh:</label>
